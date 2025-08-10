fa48f2561acf80043d4425f7ae420c1137e866c1
Ajith k v <ajith66310@gmail.com>
2025-08-10 14:56:19 +0530

    installed swiperjs for a carousel

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import Latestproducts from '../components/Latestproducts'
+import SwiperSlider from '../components/SwiperSlider.jsx'
 
 
 
@@ -11,7 +12,8 @@ const Home = () => {
 
   return (
     <>
- 
+
+   <SwiperSlider/>
     <Latestproducts/>
         <Footer/>
     </>
