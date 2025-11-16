963b01cc5db82f55f7a787b28cf835f994b7062b
Ajith k v <ajith66310@gmail.com>
2025-11-16 13:01:00 +0530

    refactor: enable withCredentials globally.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
 import { BrowserRouter } from 'react-router-dom'
+axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById('root')).render(
 <BrowserRouter>
