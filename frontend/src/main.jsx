6c5b4f28813971f18f0946526aa285c353bf110d
Ajith k v <ajith66310@gmail.com>
2025-11-16 13:04:43 +0530

    fix: axios error

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
 import { BrowserRouter } from 'react-router-dom'
+import axios from "axios";
 axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById('root')).render(
